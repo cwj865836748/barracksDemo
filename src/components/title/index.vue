4e58932b5b7102f0d90f4699061a3ce787817479
Go1d <851654538@qq.com>
2020-06-12 22:53:33 +0800

    二页面

---
+++ src/components/title/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="zy-common-header" :class="[type==2?'big':'']">
+    <div class="title">
+      {{text}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "",
+    props: {
+      text: String,
+      type: {
+        type: Number,
+        default: 1
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped type="text/scss">
+  .zy-common-header {
+    height: 50px;
+    background: url("../../assets/images/chr.png") no-repeat;
+    background-position: center bottom;
+
+    &.big {
+      background: url("../../assets/images/chr2.png") no-repeat;
+      background-position: center bottom;
+    }
+    .title {
+      font-size: 24px;
+      line-height: 50px;
+      padding: 0 25px;
+      color: #ffffff;
+      opacity: 1;
+    }
+  }
+</style>
--- src/views/situationMap.vue
+++ src/views/situationMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content" >
+  <div class="content">
     <!--头部-->
     <div class="content_head">
       <img src="@/assets/images/head.png"/>
@@ -7,33 +7,99 @@
       <div class="head_camp" @click="campShow=!campShow">{{camp}}营区<img src="@/assets/images/select.png"/></div>
       <div class="head_date">{{nowDate}}</div>
       <div class="head_choose" v-show="campShow">
-        <div :class="['camp_icon',camp==='A'?'icon':'']"  @click="chooseCamp('A')">A营区</div>
-        <div :class="['camp_icon',camp==='B'?'icon':'']"  @click="chooseCamp('B')">B营区</div>
-        <div :class="['camp_icon',camp==='C'?'icon':'']"  @click="chooseCamp('C')">C营区</div>
+        <div :class="['camp_icon',camp==='A'?'icon':'']" @click="chooseCamp('A')">A营区</div>
+        <div :class="['camp_icon',camp==='B'?'icon':'']" @click="chooseCamp('B')">B营区</div>
+        <div :class="['camp_icon',camp==='C'?'icon':'']" @click="chooseCamp('C')">C营区</div>
       </div>
     </div>
+
+    <div class="content-footer">
+      <div class="content-footer-week">
+        <zy-title text="周安全形势"/>
+        <Plain/>
+      </div>
+      <div class="content-footer-today">
+        <zy-title text="今日安全形势" :type="2"/>
+        <div class="content-footer-today-box">
+          <div class="content-footer-today-box-header">
+            <span class="title">今日安全形势</span>
+            <div class="progress-bar">
+              <span class="schedule"></span>
+              <span class="number">70%</span>
+            </div>
+            <img src="../assets/images/schedule.png" alt="">
+            <span class="desc">较昨日上升20%</span>
+          </div>
+          <div class="flex-row content-footer-today-box-footer  ">
+            <div class="flex-grow-1 flex-col flex-x-center flex-y-center">
+              <img src="../assets/images/qiu.png" alt="" width="108" height="108">
+              <span>人行安全形势</span>
+            </div>
+            <div class="flex-grow-1 flex-col flex-x-center flex-y-center">
+              <img src="../assets/images/qiu.png" alt="" width="108" height="108">
+              <span>人行安全形势</span>
+            </div>
+            <div class="flex-grow-1 flex-col flex-x-center">
+              <img src="../assets/images/qiu.png" alt="" width="108" height="108">
+              <span>人行安全形势</span>
+            </div>
+            <div class="flex-grow-1 flex-col flex-x-center">
+              <img src="../assets/images/qiu.png" alt="" width="108" height="108">
+              <span>人行安全形势</span>
+            </div>
+            <div class="flex-grow-1 flex-col flex-x-center">
+              <img src="../assets/images/qiu.png" alt="" width="108" height="108">
+              <span>人行安全形势</span>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="content-footer-month">
+        <zy-title text="月安全形势"/>
+      </div>
+      <div class="content-footer-personnel">
+        <zy-title text="人员管理"/>
+        <Pie/>
+      </div>
+      <div class="content-footer-vedio">
+        <zy-title text="周安全形势"/>
+      </div>
+      <div class="content-footer-bullet">
+        <zy-title text="枪弹管理"/>
+      </div>
+      <div class="content-footer-vehicle">
+        <zy-title text="车辆管理"/>
+        <brokenLine/>
+      </div>
+      <div class="content-footer-device">
+        <zy-title text="设备运维"/>
+      </div>
+    </div>
+
     <!--左边-->
-    <div class="content_left">
-      <div class="content_left_one">
-        <div class="myContent">
-          <div>每天安全形势</div>
-          <Plain/>
-        </div>
-      </div>
-      <div class="content_left_one">
-        <div class="myContent">
-          <div>人员管理</div>
-          <Pie/>
-        </div>
-      </div>
-      <div class="content_left_one">
-        <div class="myContent">
-          <div>车辆管理</div>
-          <brokenLine/>
-        </div>
-      </div>
-    </div>
+    <!--<div class="content_left">-->
+    <!--<div class="content_left_one">-->
+    <!--<div class="myContent">-->
+    <!--<div>每天安全形势</div>-->
+    <!--<Plain/>-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--<div class="content_left_one">-->
+    <!--<div class="myContent">-->
+    <!--<div>人员管理</div>-->
+    <!--<Pie/>-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--<div class="content_left_one">-->
+    <!--<div class="myContent">-->
+    <!--<div>车辆管理</div>-->
+    <!--<brokenLine/>-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--</div>-->
     <!--中间-->
+
     <!--右边-->
     <earthVideo class="myEarthVideo" :videoSrc="videoSrc" v-if="videoShow" @closeShow="videoShow=false"/>
 
@@ -41,55 +107,111 @@
 </template>
 
 <script>
-import { TimeFormat } from '@/utils/utils'
-import Plain from '@/components/echarts/plain'
-import Pie from '@/components/echarts/pie'
-import brokenLine from '@/components/echarts/brokenLine'
-import earthVideo from '@/components/earthVideo/earthVideo'
+  import {TimeFormat} from '@/utils/utils'
+  import Plain from '@/components/echarts/plain'
+  import Pie from '@/components/echarts/pie'
+  import brokenLine from '@/components/echarts/brokenLine'
+  import earthVideo from '@/components/earthVideo/earthVideo'
+  import zyTitle from '@/components/title/index'
+  import 'echarts-liquidfill/src/liquidFill.js';
 
-export default {
-  name: 'situationMap',
-  data () {
-    return {
-      nowDate: '',
-      timer: null,
-      intnum: null,
-      camp: 'A',
-      campShow: false,// 下拉
-      videoShow:false
-    }
-  },
-  components: {
-    Plain, Pie, brokenLine, earthVideo
-  },
-
-  created () {
-    this.getDate()
-  },
-  mounted () {
-  },
-  beforeDestroy () {
-    clearInterval(this.timer)
-    this.timer = null
-  },
-  methods: {
-    getDate () {
-      this.timer = setInterval(() => { // 创建定时器
-        var date = new Date()
-        this.nowDate = TimeFormat(date)
-      }, 1000)
+  export default {
+    name: 'situationMap',
+    data() {
+      return {
+        nowDate: '',
+        timer: null,
+        intnum: null,
+        camp: 'A',
+        campShow: false,// 下拉
+        videoShow: false,
+        temp: 10
+      }
     },
-    chooseCamp (val) {
-      this.videoSrc = `/video/${this.camp}${val}.mp4`
-      this.videoShow = true
-      this.camp = val
-      this.campShow = false
+    components: {
+      Plain, Pie, brokenLine, earthVideo,
+      zyTitle
+    },
+
+    created() {
+      this.getDate()
+      // this.drawTemperature();
+    },
+    mounted() {
+    },
+    beforeDestroy() {
+      clearInterval(this.timer)
+      this.timer = null
+    },
+    methods: {
+      getDate() {
+        this.timer = setInterval(() => { // 创建定时器
+          var date = new Date()
+          this.nowDate = TimeFormat(date)
+        }, 1000)
+      },
+      chooseCamp(val) {
+        this.videoSrc = `/video/${this.camp}${val}.mp4`
+        this.videoShow = true
+        this.camp = val
+        this.campShow = false
+      },
+      drawTemperature() {
+        let myChart = this.$echarts.init(document.getElementById('temperatrue-echarts'));
+        // window.console.log(this.temp)
+        var value = parseInt(this.temp) / 100;
+        var value1 = this.temp;
+        var data = [{
+          value: value,
+          amplitude: '5%',
+          waveLength: '80%',
+          itemStyle: {
+            color: 'rgba(52, 130, 250, 0.9)'
+          }
+        }, {
+          value: value,
+          amplitude: '5%',
+          waveLength: '80%',
+          itemStyle: {
+            color: 'rgba(52, 130, 250, 0.9)'
+          }
+        }];
+        var option = {
+          series: [{
+            type: 'liquidFill',
+            radius: '95%',
+            // cent  er: ['50%', '40%'],
+            data: data,
+            outline: {
+              show: false,
+              borderDistance: 0,
+            },
+            backgroundStyle: {
+              borderWidth: 5,
+              borderColor: 'rgba(53, 133, 255, 1)',
+              color: 'rgba(9, 14, 47, 0.76)'
+            },
+            label: {
+              normal: {
+                formatter: function () {
+                  return value1;
+                },
+                textStyle: {
+                  fontSize: 64,
+                  color: 'rgba(255, 255, 255, 1)'
+                }
+              }
+            },
+
+          }]
+        };
+        myChart.setOption(option);
+      },
     }
   }
-}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped type="text/scss">
   .content {
     position: relative;
     background: url("../assets/images/bg.png") no-repeat;
@@ -185,29 +307,164 @@ export default {
       }
     }
     .content_left {
-     .content_left_one {
-       height: 285px;
-       width: 446px;
-       background: #020028;
-       opacity: 0.7;
-       border: 3px solid #000D55;
-       .myContent {
-         padding: 10px 20px 15px;
-         font-family: Microsoft YaHei UI;
-         font-style: normal;
-         font-weight: normal;
-         font-size: 24px;
-         line-height: 30px;
-         letter-spacing: 0.15em;
-         color: #FFFFFF;
-       }
-     }
+      .content_left_one {
+        height: 285px;
+        width: 446px;
+        background: #020028;
+        opacity: 0.7;
+        border: 3px solid #000D55;
+        .myContent {
+          padding: 10px 20px 15px;
+          font-family: Microsoft YaHei UI;
+          font-style: normal;
+          font-weight: normal;
+          font-size: 24px;
+          line-height: 30px;
+          letter-spacing: 0.15em;
+          color: #FFFFFF;
+        }
+      }
     }
     .myEarthVideo {
       position: fixed;
-      top:0;
+      top: 0;
       left: 0;
       z-index: 999;
     }
+
+    &-footer {
+      padding: 20px;
+      box-sizing: border-box;
+      height: 960px;
+      position: relative;
+      background: url("../assets/images/bg.png") no-repeat;
+      background-size: 100% 100%;
+
+      &-week {
+        width: 450px;
+        height: 290px;
+        position: absolute;
+        top: 20px;
+        left: 20px;
+        border: 2px solid #000D62;
+        background: rgba(2, 0, 40, 0.7);
+      }
+
+      &-today {
+        width: 920px;
+        height: 290px;
+        position: absolute;
+        top: 20px;
+        left: 500px;
+        border: 2px solid #000D62;
+        background: rgba(2, 0, 40, 0.7);
+
+        &-box {
+          padding: 20px;
+          &-header {
+            line-height: 24px;
+            display: flex;
+            align-items: center;
+
+            .title {
+              font-size: 18px;
+              color: #ffffff;
+              padding: 0 10px 0 20px;
+            }
+
+            .desc {
+              color: #ffffff;
+              font-size: 14px;
+            }
+
+            .progress-bar {
+              width: 600px;
+              height: 24px;
+              display: inline-block;
+              background: rgba(0, 3, 37, 0.7);
+              position: relative;
+            }
+
+            .schedule {
+              position: absolute;
+              top: 0;
+              left: 0;
+              background: #FF2756;
+              width: 420px;
+              height: 24px;
+            }
+            .number {
+              position: absolute;
+              top: 0;
+              left: 430px;
+              color: #ffffff;
+              font-size: 14px;
+            }
+          }
+          &-footer {
+          }
+        }
+      }
+
+      &-month {
+        width: 450px;
+        height: 290px;
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        border: 2px solid #000D62;
+        background: rgba(2, 0, 40, 0.7);
+      }
+
+      &-personnel {
+        width: 450px;
+        height: 290px;
+        position: absolute;
+        top: 335px;
+        left: 20px;
+        border: 2px solid #000D62;
+        background: rgba(2, 0, 40, 0.7);
+      }
+
+      &-vedio {
+        width: 920px;
+        height: 605px;
+        background: green;
+        position: absolute;
+        top: 335px;
+        left: 500px;
+      }
+
+      &-bullet {
+        width: 450px;
+        height: 290px;
+        background: green;
+        position: absolute;
+        top: 335px;
+        right: 20px;
+      }
+
+      &-vehicle {
+        width: 450px;
+        height: 290px;
+        position: absolute;
+        bottom: 20px;
+        left: 20px;
+        border: 2px solid #000D62;
+        background: rgba(2, 0, 40, 0.7);
+      }
+
+      &-device {
+        width: 450px;
+        height: 290px;
+        background: green;
+        position: absolute;
+        bottom: 20px;
+        right: 20px;
+      }
+
+    }
   }
+
+
 </style>
